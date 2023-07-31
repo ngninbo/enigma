d3c3431a56ad25797787f859313b8d00b4520af9
Beauclair Dongmo Ngnintedem <beauclair.dongmongnintedem@cofinpro.de>
2023-07-31 07:21:18 +0200

    completed stage 6/6

---
+++ enigma.sh
@@ -0,0 +1,209 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2162
+welcome="Welcome to the Enigma!"
+filename_msg="Enter the filename:"
+file_not_found="File not found!"
+z=90
+a=65
+#alphabet='A-Z'
+#shifted_alphabet="D-ZA-C"
+space=32
+key=3
+
+function out_menu() {
+  options=("Exit" "Create a file" "Read a file" "Encrypt a file" "Decrypt a file")
+
+  for ((i = 0; i < ${#options[@]}; i++)); do
+    printf "%s. %s\n" "$i" "${options[i]}"
+  done
+}
+
+function main() {
+  printf "%s\n\n" "$welcome"
+
+  while
+    true
+
+    out_menu
+    echo "Enter an option:" && read option
+  do
+
+    case $option in
+    0) echo "See you later!" && exit;;
+    1) create_file ;;
+    2) read_file ;;
+    3) encrypt_file ;;
+    4) decrypt_file ;;
+    *)
+      echo "Invalid option!"
+      ;;
+    esac
+  done
+}
+
+function create_file() {
+  printf "%s\n" "$filename_msg" && read filename
+  if [[ "$filename" =~ ^[a-zA-Z.]+$ ]]; then
+    echo "Enter a message:" && read msg
+    if [[ $(check_input "$msg") == true ]]; then
+      echo "$msg" >> "$filename"
+      printf "The file was created successfully!\n\n"
+    else
+      printf "This is not a valid message!\n\n"
+    fi
+  else
+    printf "File name can contain letters and dots only!\n\n"
+  fi
+}
+
+function read_file() {
+  printf "%s\n" "$filename_msg" && read filename
+  if [[ -f "$filename" ]]; then
+    printf "File content:\n%s\n\n" "$(cat "$filename")"
+  else
+    printf "%s\n\n" "$file_not_found"
+  fi
+}
+
+function encrypt_file() {
+  printf "%s\n" "$filename_msg" && read file_name
+  if [[ -f "$file_name" ]]; then
+    echo "Enter password:" && read password
+    output_file="$(printf "%s.enc" "$file_name")"
+    openssl enc -aes-256-cbc -e -pbkdf2 -nosalt -in "$file_name" -out "$output_file" -pass pass:"$password" &>/dev/null
+    exit_code=$?
+
+    if [[ $exit_code -ne 0 ]]; then
+      echo "Fail"
+    else
+      rm "$file_name"
+      printf "Success\n\n"
+    fi
+  else
+    printf "%s\n\n" "$file_not_found"
+  fi
+}
+
+function decrypt_file() {
+  printf "%s\n" "$filename_msg" && read file_name
+  if [[ -f "$file_name" ]]; then
+    echo "Enter password:" && read password
+    name=$(echo "$file_name" | cut -d '.' -f 1)
+    output_file="$(printf "%s.txt" "$name")"
+    openssl enc -aes-256-cbc -d -pbkdf2 -nosalt -in "$file_name" -out "$output_file" -pass pass:"$password" &>/dev/null
+    exit_code=$?
+
+    if [[ $exit_code -ne 0 ]]; then
+      echo "Fail"
+    else
+      rm "$file_name"
+      printf "Success\n\n"
+    fi
+  else
+    printf "%s\n\n" "$file_not_found"
+  fi
+}
+
+function encryption() {
+  printf "Type 'e' to encrypt, 'd' to decrypt a message:\nEnter a command:\n" && read command
+  if [[ "$command" =~ ^[d-e]$ ]]; then
+    echo "Enter a message:" && read message
+    if [[ $(check_input "$message") == true ]]; then
+      decrypt_or_encrypt_text "$message" "$command"
+    else
+      echo "This is not a valid message!" && exit
+    fi
+  else
+    printf "Invalid command!\n\n"
+  fi
+}
+
+function decrypt_or_encrypt_text() {
+  case $2 in
+  e) encrypt_text "$1" ;;
+  d) decrypt_text "$1" ;;
+  *) ;;
+
+  esac
+}
+
+function encrypt_text() {
+  message=$1
+  result=""
+  for ((i = 0; i < ${#message}; i++)); do
+    char=${message:$i:1}
+    value=$(val "$char")
+    if [[ $value -eq $space ]]; then
+      result=$(printf "%s%s" "$result" "$char")
+    else
+      result=$(printf "%s%s" "$result" "$(shift_letter_right "$char" $key)")
+    fi
+  done
+  #printf "Encrypted message:\n%s\n" "$(echo "$1" | tr $alphabet $shifted_alphabet)"
+  printf "%s\n" "$result"
+}
+
+function decrypt_text() {
+  message=$1
+  result=""
+  for ((i = 0; i < ${#message}; i++)); do
+    char=${message:$i:1}
+    value=$(val "$char")
+    if [[ $value -eq $space ]]; then
+      result=$(printf "%s%s" "$result" "$char")
+    else
+      result=$(printf "%s%s" "$result" "$(shift_letter_left "$char" $key)")
+    fi
+  done
+  #printf "Decrypted message:\n%s\n" "$(echo "$1" | tr $shifted_alphabet $alphabet)"
+  printf "%s\n" "$result"
+}
+
+function check_input() {
+  re='^[A-Z ]+$'
+  [[ "$1" =~ $re ]] && echo true || echo false
+}
+
+function is_uppercase_letter() {
+  [[ "$1" =~ ^[A-Z]$ ]] && echo true || echo false
+}
+
+function is_digit() {
+  [[ "$1" =~ ^[0-9]$ ]] && echo true || echo false
+}
+
+function shift_letter_right() {
+  value=$(val "$1")
+
+  for ((i = 1; i <= $2; i++)); do
+    ((value++))
+    if [ "$value" -gt $z ]; then
+      value=$a
+    fi
+  done
+
+  printf "%s" "$(character "$value")"
+}
+
+function shift_letter_left() {
+  value=$(val "$1")
+
+  for ((i = 1; i <= $2; i++)); do
+    ((value--))
+    if [ "$value" -lt $a ]; then
+      value=$z
+    fi
+  done
+
+  printf "%s" "$(character "$value")"
+}
+
+function character() {
+  printf "%b" "$(printf "\\%03o" "$1")"
+}
+
+function val() {
+  printf "%d\n" "'$1"
+}
+
+main
